239d7c03fefeb2ac91e9fa8f820cf4eab4e4499e
Marwan Esam <marwanesam60@gmail.com>
2026-08-01 19:31:04 +0300

    chore(security): add Vercel production domain to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:3000"],
+  allow_origins=["http://localhost:3000", "https://media-transcoding-site.vercel.app"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
